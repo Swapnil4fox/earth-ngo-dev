14e1bb861582c8a3d10814bd28ab918c00c9c32b
Swapnil Yashod <swapnil.4fox@gmail.com>
2021-11-26 18:39:38 +0530

    header foooter commom

---
+++ application/views/footer.php
@@ -0,0 +1,74 @@
+<footer class="col-100 floatLft flexDisplay justifyCenter">
+        <div class="wrapper">
+            <div class="ap__footInner col-100 floatLft flexDisplay justifySpace alignStart flexWrap">
+                <div class="ap__footLogo">
+                    <div class="logoDiv"><img src="./images/logo-1.png" alt=""></div>
+                    <p class="ap__whtPara ap__common__para">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
+                </div>
+                <div class="ap__footMenuWrp">
+                    <div class="ap__footMenu">
+                        <h3 class="ap__footHead">Quick Links</h3>
+                        <ul class="ap__footNav col-100 floatLft">
+                            <li>
+                                <a href="index.html">Home</a>
+                            </li>
+                            <li>
+                                <a href="about-us.html">About us</a>
+                            </li>
+                            <li>
+                                <a href="gallary.html">Gallery</a>
+                            </li>
+                            <li>
+                                <a href="contactus.html">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="ap__footMenu">
+                        <h3 class="ap__footHead">Other Pages</h3>
+                        <ul class="ap__footNav col-100 floatLft">
+                            <li>
+                                <a href="javascript:;">Privacy & Policy</a>
+                            </li>
+                            <li>
+                                <a href="javascript:;">Terms of Use</a>
+                            </li>
+                            <li>
+                                <a href="javascript:;">Disclaimer</a>
+                            </li>
+                            <li>
+                                <a href="javascript:;">FAQs</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="ap__footMenu">
+                        <h3 class="ap__footHead">Contact Info</h3>
+                        <ul class="ap__footNav col-100 floatLft">
+                            <li class="flexDisplay justifyStart alignStart">
+                                <div class="ap__footIco"><img src="./images/map.png" alt=""></div>
+                                <div class="ap__addText ap__whtPara">301, 3rd Floor,Tirupati Co-operative Housing Society Ltd, Opp. Mumbai University Main Gate, Vidyanagari Marg, (CST Road), Kalina, Santacruz (East), Mumbai - 400 098.</div>
+                            </li>
+                            <li class="flexDisplay justifyStart alignCenter">
+                                <div class="ap__footIco"><img src="./images/call.png" alt=""></div>
+                                <div class="ap__addText"><a href="tel:+91 84969 84969">+91 84969 84969</a></div>
+                            </li>
+                            <li class="flexDisplay justifyStart alignCenter">
+                                <div class="ap__footIco"><img src="./images/email-small.png" alt=""></div>
+                                <div class="ap__addText"><a href="mailto:connect@earthngo.org">connect@earthngo.org</a></div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="ap__footCopy col-100 floatLft flexDisplay justifySpace">
+                <p class="ap__whtPara">Environmental & NGO Template Kit by Jegtheme</p>
+                <p class="ap__whtPara">© 2021. Company Name. All Rights Reserved.</p>
+            </div>
+        </div>
+    </footer>
+
+    <script src="./js/jQuery/jquery-3.6.min.js"></script>
+    <script src="./js/slick-slider/slick.min.js"></script>
+    <script src="./js/magnific/jquery.magnific-popup.min.js"></script>
+    <script src="./js/site.ap.js"></script>
+</body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Earth NGO</title>
+      <link rel="icon" href="./images/thumb.png" sizes="32x32">
+      <link rel="stylesheet" href="./css/font-awesome.min.css">
+      <link rel="stylesheet" href="./js/slick-slider/slick.css">
+      <link rel="stylesheet" href="./js/magnific/magnific-popup.css">
+      <link rel="stylesheet" href="./css/init.css">
+      <link rel="stylesheet" href="./css/nstyle.css">
+      <link rel="stylesheet" href="./css/astyle.css">
+   </head>
+   <body>
+      <header class="col-100 floatLft flexDisplay justifyCenter deskHeader">
+         <div class="header__wrapper">
+            <div class="headWrp col-100 floatLft flexDisplay alignCenter justifySpace">
+               <div class="ap__logoDiv floatLft"><a href="index.html"><img src="./images/logo-1.png" alt="Logo" /></a></div>
+               <nav class="floatRgt">
+                  <ul class="ap__navigation">
+                     <li class="ap__navLinks inlineBlk active">
+                        <a href="index.html" class="ap__navClick">Home</a>
+                     </li>
+                     <li class="ap__navLinks inlineBlk ">
+                        <a href="about-us.html" class="ap__navClick">About us</a>
+                     </li>
+                     <li class="ap__navLinks ap__dropParent inlineBlk relative">
+                        <a href="event-listing.html" class="ap__navClick">
+                           Events  <!-- <i class="fa fa-angle-down"></i>-->
+                        </a>
+                        <!-- <div class="ap__dropMenu absolute">
+                           <div class="ap__emptyHeight col-100 floatLft"></div>
+                           <div class="ap__dropInner col-100 floatLft">
+                               <a href="javascript:;" class="ap__dropInnerLink">Event Details</a>
+                               <a href="javascript:;" class="ap__dropInnerLink">Event Gallery</a>
+                           </div>
+                           </div> -->
+                     </li>
+                     <li class="ap__navLinks ap__dropParent inlineBlk relative">
+                        <a href="campaign-listing.html" class="ap__navClick">
+                           Campaign  <!-- <i class="fa fa-angle-down"></i>-->
+                        </a>
+                        <!-- <div class="ap__dropMenu absolute">
+                           <div class="ap__emptyHeight col-100 floatLft"></div>
+                           <div class="ap__dropInner col-100 floatLft">
+                               <a href="javascript:;" class="ap__dropInnerLink">Campaign Details</a>
+                               <a href="javascript:;" class="ap__dropInnerLink">Campaign Gallery</a>
+                           </div>
+                           </div> -->
+                     </li>
+                     <li class="ap__navLinks inlineBlk ">
+                        <a href="gallary.html" class="ap__navClick">Gallery</a>
+                     </li>
+                     <li class="ap__navLinks inlineBlk">
+                        <a href="volunteer.html" class="ap__navClick">Volunteer</a>
+                     </li>
+                     <li class="ap__navLinks inlineBlk">
+                        <a href="corporate-partnership.html" class="ap__navClick">Corporate partnership</a>
+                     </li>
+                     <li class="ap__navLinks inlineBlk">
+                        <a href="sustain-goals.html" class="ap__navClick">Sustainable Goals</a>
+                     </li>
+                     <li class="ap__navLinks inlineBlk">
+                        <a href="contactus.html" class="ap__navClick">Contact us</a>
+                     </li>
+                     <li class="ap__navLinks inlineBlk">
+                        <a href="donate.html" class="ap__navClick ap__navButton">Donation</a>
+                     </li>
+                  </ul>
+               </nav>
+            </div>
+         </div>
+      </header>
+      <div class="headerMob">
+         <div class="logoDiv">
+            <a href="index.html"><img src="./images/logo-1.png" alt=""></a>
+         </div>
+         <span class="toggle-button">
+            <div class="menu-bar menu-bar-top"></div>
+            <div class="menu-bar menu-bar-middle"></div>
+            <div class="menu-bar menu-bar-bottom"></div>
+         </span>
+         <div class="menu-wrap">
+            <div class="menu-sidebar">
+               <div class="navMenu">
+                  <ul class="navMenu">
+                     <li class="ap__navLinks inlineBlk mainDrop ">
+                        <a href="index.html" class="ap__navClick active">Home</a>
+                     </li>
+                     <li class="ap__navLinks inlineBlk mainDrop">
+                        <a href="about-us.html" class="ap__navClick ">About us</a>
+                     </li>
+                     <li class="ap__navLinks ap__dropParent inlineBlk relative mainDrop">
+                        <a href="event-listing.html" class="ap__navClick">
+                           Events <!--<i class="fa fa-angle-down"> --></i>
+                        </a>
+                        <!-- <div class="ap__dropMenu ap__mobile relative">
+                           <div class="ap__emptyHeight col-100 floatLft"></div>
+                           <div class="ap__dropInner col-100 floatLft">
+                               <a href="javascript:;" class="ap__dropInnerLink">Event Details</a>
+                               <a href="javascript:;" class="ap__dropInnerLink">Event Gallery</a>
+                           </div>
+                           </div> -->
+                     </li>
+                     <li class="ap__navLinks ap__dropParent inlineBlk relative mainDrop">
+                        <a href="campaign-listing.html" class="ap__navClick">
+                           Campaign <!--<i class="fa fa-angle-down"> --></i>
+                        </a>
+                        <!-- <div class="ap__dropMenu ap__mobile relative">
+                           <div class="ap__emptyHeight col-100 floatLft"></div>
+                           <div class="ap__dropInner col-100 floatLft">
+                               <a href="javascript:;" class="ap__dropInnerLink">Campaign Details</a>
+                               <a href="javascript:;" class="ap__dropInnerLink">Campaign Gallery</a>
+                           </div>
+                           </div> -->
+                     </li>
+                     <li class="ap__navLinks inlineBlk mainDrop ">
+                        <a href="gallary.html" class="ap__navClick">Gallery</a>
+                     </li>
+                     <li class="ap__navLinks inlineBlk mainDrop">
+                        <a href="volunteer.html" class="ap__navClick">Volunteer</a>
+                     </li>
+                     <li class="ap__navLinks inlineBlk mainDrop">
+                        <a href="corporate-partnership.html" class="ap__navClick">Corporate partnership</a>
+                     </li>
+                     <li class="ap__navLinks inlineBlk mainDrop ">
+                        <a href="sustain-goals.html" class="ap__navClick ">Sustainable Goals</a>
+                     </li>
+                     <li class="ap__navLinks inlineBlk mainDrop">
+                        <a href="contactus.html" class="ap__navClick">Contact us</a>
+                     </li>
+                     <li class="ap__navLinks inlineBlk mainDrop">
+                        <a href="donate.html" class="ap__navClick ap__navButton">Donation</a>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+         </div>
+      </div>
